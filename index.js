668c4157e292e70c11f772b48e82701c938e92c9
MorganRedgrove <morganekelunddavies@gmail.com>
2022-12-14 14:55:41 +0000

    remove console.log

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 function updateLocation(array) {
     return array?.map(element => {
-        console.log(element.location)
         if (element.location === undefined) {
             element.location = "remote"
         }
